74ffc46e2d0caa5fcff8eae6884cacbc53ba2052
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2020-05-28 20:31:55 -0500

    agregar campos id y fecha de publicacion a peticiones get

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,3 +1,5 @@
+# Standard library
+from datetime import datetime
 # Django Rest Framework
 from rest_framework import serializers
 
@@ -6,12 +8,19 @@ from blog.models import Entrada
 
 class EntradaModelSerializer(serializers.ModelSerializer):
 
+    fecha_publ = serializers.SerializerMethodField()
+
+    def get_fecha_publ(self, obj: Entrada) -> datetime:
+        return datetime.timestamp(obj.fecha_publ)
 
     class Meta:
         model = Entrada
         fields = (
+            'id',
             'titulo',
             'texto_intro',
             'texto_completo',
             'imagen',
+            'fecha_publ'
         )
+        read_only_fields = ('id', 'fecha_publ')
