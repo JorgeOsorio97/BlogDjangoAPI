540166c6723b0f9937b8a9bde63f71e92d44ef70
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2020-05-31 22:51:06 -0500

    coreccion CORS

--- BlogAPI/settings.py
+++ BlogAPI/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = (
-    'localhost:3000',
-    '127.0.0.1:3000',
-    '*'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
 )
